a7586b33746ae19e9c744a155b588481a619361a
Austin Paul <safeat3rd@hotmail.com>
2017-11-13 23:31:41 -0600

    MainActivity complete

--- app/src/main/java/aoap/myapplication/MainActivity.java
+++ app/src/main/java/aoap/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package aoap.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button makeReservationButton = findViewById(R.id.new_reservation_button);
+        Button viewReservationButton = findViewById(R.id.view_reservation_button);
+
+        View.OnClickListener makeReservationListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent toDishIntent = new Intent(MainActivity.this, DishActivity.class);
+                startActivity(toDishIntent);
+            }
+        };
+
+        View.OnClickListener viewReservationListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent toViewReservationIntent = new Intent(MainActivity.this, DishActivity.class);
+                startActivity(toViewReservationIntent);
+            }
+        };
+
+        makeReservationButton.setOnClickListener(makeReservationListener);
+        viewReservationButton.setOnClickListener(viewReservationListener);
+
     }
 }
