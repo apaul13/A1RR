4798209222df6c0367914b40044fa4abe7dafb19
Austin Paul <safeat3rd@hotmail.com>
2017-11-14 23:00:15 -0600

    Started DishActivity, modified Name and Time layout

--- app/src/main/java/aoap/myapplication/DishActivity.java
+++ app/src/main/java/aoap/myapplication/DishActivity.java
@@ -2,6 +2,10 @@ package aoap.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DishActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class DishActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dish);
+
+        Button totalButton = findViewById(R.id.total_button);
+
+
+        View.OnClickListener totalListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                EditText priceET = findViewById(R.id.unit_price_et);
+                EditText quantityET = findViewById(R.id.quantity_et);
+                TextView totalTV = findViewById(R.id.total_tv);
+
+                double price = Double.parseDouble(priceET.getText().toString());
+                int quantity = Integer.parseInt(quantityET.getText().toString());
+                double total = price * quantity;
+
+                totalTV.setText("$" + total);
+            }
+        };
+
+        totalButton.setOnClickListener(totalListener);
     }
 }
