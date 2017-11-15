b2f792919287c1da9e70b2f0f9f7c8a6e630c4f1
artur oganezov <artur@arturs-MacBook-Pro.local>
2017-11-15 00:08:46 -0600

    making a god damn circle

--- app/src/main/java/aoap/myapplication/TableActivity.java
+++ app/src/main/java/aoap/myapplication/TableActivity.java
@@ -10,40 +10,40 @@ import android.view.View;
 
 public class TableActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_table);
-//
-//
-//    }
-//}
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
-    }
+        setContentView(R.layout.activity_table);
 
-    public class MyView extends View {
-        public MyView(Context context) {
-            super(context);
-        }
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            int x = getWidth();
-            int y = getHeight();
-            int radius;
-            radius = 100;
-            Paint paint = new Paint();
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.WHITE);
-            canvas.drawPaint(paint);
-            paint.setColor(Color.parseColor("#da4747"));
-            canvas.drawCircle(x/2,y/2,radius,paint);
-        }
     }
 }
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(new MyView(this));
+//    }
+//
+//    public class MyView extends View {
+//        public MyView(Context context) {
+//            super(context);
+//        }
+//
+//        @Override
+//        protected void onDraw(Canvas canvas) {
+//            super.onDraw(canvas);
+//            int x = getWidth();
+//            int y = getHeight();
+//            int radius;
+//            radius = 100;
+//            Paint paint = new Paint();
+//            paint.setStyle(Paint.Style.FILL);
+//            paint.setColor(Color.WHITE);
+//            canvas.drawPaint(paint);
+//            paint.setColor(Color.parseColor("#da4747"));
+//            canvas.drawCircle(x/2,y/2,radius,paint);
+//        }
+//    }
+//}
+//
