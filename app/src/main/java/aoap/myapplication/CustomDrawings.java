6877c8e26929690577e8340dc83ece95fba3f79e
artur oganezov <artur@arturs-MacBook-Pro.local>
2017-11-21 14:51:57 -0600

    comments added

--- app/src/main/java/aoap/myapplication/CustomDrawings.java
+++ app/src/main/java/aoap/myapplication/CustomDrawings.java
@@ -1,6 +1,5 @@
 package aoap.myapplication;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Canvas;
@@ -11,18 +10,14 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.graphics.drawable.shapes.OvalShape;
 import android.graphics.Paint;
-import android.widget.Toast;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
 import java.util.Map;
 
 
@@ -31,15 +26,13 @@ import java.util.Map;
  */
 
 public class CustomDrawings extends View {
-    Paint paint1, paint2, paint3, paint4, paint5, paint6, paint7, paint8;
-    Paint[] paints = {paint1, paint2, paint3, paint4, paint5, paint6, paint7, paint8};
-    int width = this.getWidth();
+    Paint paint1, paint2, paint3, paint4, paint5, paint6, paint7, paint8; //colors for the tables
+    int width = this.getWidth(); //width and height of the screen
     int height = this.getHeight();
-    Rect r1, r2, r3, r4, r5, r6, r7, r8;
-    Context context;
+    Rect r1, r2, r3, r4, r5, r6, r7, r8; //the 8 tables
+    Context context; //context to be grabbed from TableActivity. Needed to use intent.
     Intent intent;
     String tableNum;
-    private int c1 = Color.GREEN;
 
     public CustomDrawings(Context context) {
         super(context);
@@ -62,26 +55,26 @@ public class CustomDrawings extends View {
     }
 
     public void init(@Nullable AttributeSet set) {
+        //intnent was to use firebase here to check if the table is already taken.
         FirebaseDatabase theDB;
         DatabaseReference dbRef;
         theDB = FirebaseDatabase.getInstance();
         dbRef = theDB.getReference().child("Tables");
-        final int c2=Color.GREEN,c3=Color.GREEN,c4=Color.GREEN,c5=Color.GREEN,c6=Color.GREEN,c7=Color.GREEN,c8=Color.GREEN;
-
-
 
         dbRef.addValueEventListener(new ValueEventListener() { //attach listener
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) { //something changed!
                 Map<String,String> tableMap = (Map<String,String>) dataSnapshot.getValue();
                 Log.i("test", "DATASNAPSHOT: "+tableMap.get("Table 1"));
-                c1 = Color.RED;
                 invalidate();
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) { }
         });
+
+        //sets all the tables to green, as in free.
+        final int c1=Color.GREEN,c2=Color.GREEN,c3=Color.GREEN,c4=Color.GREEN,
+                c5=Color.GREEN,c6=Color.GREEN,c7=Color.GREEN,c8=Color.GREEN;
 
         paint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -107,7 +100,7 @@ public class CustomDrawings extends View {
         width = this.getWidth();
         height = this.getHeight();
 
-        //left,top,right,bottom
+        //creates all the tables.
         r1 = new Rect(250, 150, 550, 450); //top left
         canvas.drawRect(r1, paint1);
         r8 = new Rect(width - 550, height - 450, width - 250, height - 150); //bottom right
@@ -137,42 +130,41 @@ public class CustomDrawings extends View {
             if (r1.contains(touchX, touchY)) {
                 tableNum = "1";
                 //myRef.child("Table 1").setValue("true");
-                paint1.setColor(paint1.getColor() == Color.GREEN ? Color.WHITE : Color.GREEN);
+                paint1.setColor(paint1.getColor() == Color.GREEN ? Color.RED : Color.GREEN);
             } else if (r2.contains(touchX, touchY)) {
                 tableNum = "2";
                 //myRef.child("Table 2").setValue("true");
-                paint2.setColor(paint2.getColor() == Color.GREEN ? Color.WHITE : Color.GREEN);
+                paint2.setColor(paint2.getColor() == Color.GREEN ? Color.RED : Color.GREEN);
             } else if (r3.contains(touchX, touchY)) {
                 tableNum = "3";
                 //myRef.child("Table 3").setValue("true");
-                paint3.setColor(paint3.getColor() == Color.GREEN ? Color.WHITE : Color.GREEN);
+                paint3.setColor(paint3.getColor() == Color.GREEN ? Color.RED : Color.GREEN);
             } else if (r4.contains(touchX, touchY)) {
                 tableNum = "4";
                 //myRef.child("Table 4").setValue("true");
-                paint4.setColor(paint4.getColor() == Color.GREEN ? Color.WHITE : Color.GREEN);
+                paint4.setColor(paint4.getColor() == Color.GREEN ? Color.RED : Color.GREEN);
             } else if (r5.contains(touchX, touchY)) {
                 tableNum = "5";
                 //myRef.child("Table 5").setValue("true");
-                paint5.setColor(paint5.getColor() == Color.GREEN ? Color.WHITE : Color.GREEN);
+                paint5.setColor(paint5.getColor() == Color.GREEN ? Color.RED : Color.GREEN);
             } else if (r6.contains(touchX, touchY)) {
                 tableNum = "6";
                 //myRef.child("Table 6").setValue("true");
-                paint6.setColor(paint6.getColor() == Color.GREEN ? Color.WHITE : Color.GREEN);
+                paint6.setColor(paint6.getColor() == Color.GREEN ? Color.RED : Color.GREEN);
             } else if (r7.contains(touchX, touchY)) {
                 tableNum = "7";
                 //myRef.child("Table 7").setValue("true");
-                paint7.setColor(paint7.getColor() == Color.GREEN ? Color.WHITE : Color.GREEN);
+                paint7.setColor(paint7.getColor() == Color.GREEN ? Color.RED : Color.GREEN);
             } else if (r8.contains(touchX, touchY)) {
                 tableNum = "8";
                 //myRef.child("Table 8").setValue("true");
-                paint8.setColor(paint8.getColor() == Color.GREEN ? Color.WHITE : Color.GREEN);
+                paint8.setColor(paint8.getColor() == Color.GREEN ? Color.RED : Color.GREEN);
             }
-            invalidate();
+            invalidate(); //changes color of selected square.
 
-            context = TableActivity.context;
+            context = TableActivity.context; //needed to use intent in non-activity
             intent = new Intent(context, TableActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            Log.i("test", "intent starting");
             intent.putExtra("tab", tableNum);
 
             context.startActivity(intent);
--- app/src/main/java/aoap/myapplication/TableActivity.java
+++ app/src/main/java/aoap/myapplication/TableActivity.java
@@ -3,27 +3,17 @@ package aoap.myapplication;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 import android.app.AlertDialog;
-import android.app.Activity;
-import android.app.AlertDialog.Builder;
 
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.*;//DatabaseReference;
+import com.google.firebase.database.*;
 
 
 public class TableActivity extends AppCompatActivity {
-    static Context context; //static?
-    static String dish;
-    static String dishPrice;
+    static Context context; //necessary for the customDrawings class to be able to access it.
 
     private FirebaseDatabase theDB;
     private DatabaseReference dbRef;
@@ -34,30 +24,32 @@ public class TableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_table);
         context = this.getBaseContext();
 
+        //Was used to add all tables to the database.
         theDB = FirebaseDatabase.getInstance();
         dbRef = theDB.getReference().child("Tables");
 
-
+        //Intent to get everything from the customDrawings class.
         Intent getCustomIntent = getIntent();
         Bundle bdCustom = getCustomIntent.getExtras();
 
         if(bdCustom != null)
         {
             final String tableNum = (String) bdCustom.get("tab");
-
+            //If a tableNumber already exists, the user has selected a table.
             if (tableNum != null) {
+                //Ask the user to confirm their table.
                 new AlertDialog.Builder(this).setMessage("Are you sure you'd like table "+tableNum+"?").setTitle("Confirmation")
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                               // Toast.makeText(TableActivity.this, "Yaay", Toast.LENGTH_SHORT).show();
-                                //
+                                Toast.makeText(TableActivity.this, "Table "+tableNum+" confirmed.", Toast.LENGTH_SHORT).show();
+                                //If confirmed, go to DishActivity, and takes the table number too.
                                 Intent toDishIntent = new Intent(TableActivity.this, DishActivity.class);
                                 toDishIntent.putExtra("tab", tableNum);
                                 startActivity(toDishIntent);
                             }
                         })
-                        .setNegativeButton(android.R.string.no, null).show();
+                        .setNegativeButton(android.R.string.no, null).show(); //nothing happens if don't like the table you chose.
             }
         }
     }
